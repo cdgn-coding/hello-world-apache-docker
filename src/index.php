691813cae322fee6f743fc6f4c582042e352c6ef
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2022-05-01 10:31:18 -0300

    First commit

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<html lang="es"> 
+<head> 
+  <meta charset="utf-8"> 
+  <title>Nombre y apellido del alumno - App en Docker</title> 
+</head> 
+ 
+<body> 
+  <h1><?php echo "App php en Docker";?></h1> 
+  <p>Mi primera app en un contendor Docker</p> 
+</body> 
+</html>
